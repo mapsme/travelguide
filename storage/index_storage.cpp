8ef1715d9e124035d013c29663cbf433623fe88d
ExMix <rahuba.youri@gmail.com>
2013-08-05 13:39:34 +0300

    Warnings fix

--- storage/index_storage.cpp
+++ storage/index_storage.cpp
@@ -5,10 +5,10 @@ void IndexStorageMock::QueryArticleInfos(vector<ArticleInfoId> & out,  string co
 {
   out.clear();
   if (prefix.empty())
-    for (int i = 0; i < 3; ++i)
+    for (uint32_t i = 0; i < 3; ++i)
       out.push_back(i);
   else if (prefix == "L")
-    for (int i = 0; i < 2; ++i)
+    for (uint32_t i = 0; i < 2; ++i)
       out.push_back(i);
   else if (prefix.size() <= 6 && string("London").substr(prefix.size()) == prefix)
       out.push_back(1);
