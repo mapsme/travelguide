dcf362b6426192af93bad73dd4a60c04a201291f
ExMix <rahuba.youri@gmail.com>
2013-08-05 16:05:23 +0300

    add fake thumbnail for preview

--- storage/article_info_storage.cpp
+++ storage/article_info_storage.cpp
@@ -5,18 +5,20 @@ bool ArticleInfoStorageMock::GetArticleInfoById(ArticleInfo & out, ArticleInfoId
   if (id > 2)
     return false;
 
-  out.m_thumbnailUrl = "stub_url.png";
   switch (id)
   {
   case 0:
+    out.m_thumbnailUrl = "london.jpg";
     out.m_url = "London";
     out.m_title = "London";
     return true;
   case 1:
+    out.m_thumbnailUrl = "lancaster.jpg";
     out.m_url = "Lancaster";
     out.m_title = "Lancaster";
     return true;
   case 2:
+    out.m_thumbnailUrl = "great_britain.jpg";
     out.m_url = "Great_Britain";
     out.m_title = "Great Britain";
     return true;
