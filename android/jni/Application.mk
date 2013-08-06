11c93c1e0b18137f26c10cebfa1f8afe3f67f9ce
d-kunin <mr.kunin@gmail.com>
2013-08-06 19:21:03 +0300

    [and] Compile for all archs.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_STL := gnustl_static
 APP_PLATFORM := android-9
+APP_ABI := all
 
 LOCAL_PATH := $(call my-dir)
 APP_CFLAGS += -I$(LOCAL_PATH)/../../3rdparty/boost
-
 APP_GNUSTL_FORCE_CPP_FEATURES := exceptions rtti
