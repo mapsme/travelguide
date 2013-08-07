0ab14dc653503f7214c2b9c559c1369369821f31
ExMix <rahuba.youri@gmail.com>
2013-08-07 20:07:39 +0300

    rewrite cross reference links

--- builder/htmlprocessor/processor.sh
+++ builder/htmlprocessor/processor.sh
@@ -1,5 +1,6 @@
 
 for interval in 0 1 2 3 4 5 6 7;
-  do python strip.py $1 $2 $3 $interval 8 &
+  do python strip.py $1 $2 $3 $4 $5 $interval 8 &
   done
+wait
   
--- builder/htmlprocessor/strip.py
+++ builder/htmlprocessor/strip.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 import urllib
+import shutil
 from bs4 import BeautifulSoup
 
 reload(sys)
@@ -51,47 +53,99 @@ def cleanUp(soup):
 
   return content
 
+def transformStringWithEncoding(str):
+  return urllib.unquote(str.decode("latin-1").encode("utf-8"))
+
+def transformString(s):
+  unquoted = urllib.unquote(str(s));
+  for i in u"\"',/\\@#$%^&*()!~`«»":
+    unquoted = unquoted.replace(i, "_")
+  unquoted = unquoted.strip("_")
+  return unicode(unquoted.lower())
+
 def imageExist(fileName):
   global imageFiles
-  return urllib.unquote(fileName).lower() in imageFiles
+  global imageSet
+  unquotedName = transformString(fileName)
+  if unquotedName in imageFiles:
+    imageSet.add(unquotedName)
+    return True
+
+  return False
 
 
 def rewriteImages(soup):
   imgTag = soup.findAll("img");
 
   for imgElement in imgTag:
-    del imgElement['alt']
+    del imgElement["alt"]
     #todo rewrite srcset attr if we can get callback on image loading in webview
-    del imgElement['srcset']
-
+    del imgElement["srcset"]
+    
     index = -1
-    if "thumb" in imgElement['src'] and not '.pdf' in imgElement['src'].split("/")[-2]:
+    srcPath = imgElement["src"]
+    splitedSrc = srcPath.split("/")
+    if "thumb" in srcPath and not ".pdf" in splitedSrc[-2]:
       index = -2
-    imageName = imgElement['src'].split("/")[index]
+    imageName = splitedSrc[index]
     if imageExist(imageName):
-      imgElement['src'] = "images/" + imageName
+      imgElement['src'] = "images/" + transformString(imageName)
     else:
-      [s.decompose() for s in imgElement.fetchParents("div", {"class" : "thumb tright"})]
+      [s.decompose() for s in imgElement.fetchParents("div", {"class" : ["thumb tright", "thumbinner", "image"]})]
+
+def rewriteCrossLinks(soup):
+  global idMapping
+  global redirectMapping
+  links = soup.findAll("a")
+  for link in links:      
+    destTitle = link["href"].split("/",2)[-1]
+    destTitle = transformStringWithEncoding(destTitle)
+    destTitle = redirectMapping.get(destTitle, destTitle);
+    if destTitle in idMapping:
+      link["href"] = idMapping.get(destTitle, link["href"]) + ".html"
+    elif "/wiki/" in link["href"]:
+      if "File:" in link["href"]:
+        link["href"] = link.find("img")["src"]
+    else:
+        if link.string:
+          link.replace_with(link.string)
+        else:
+          link.replace_with("")
+
 
 def writeHtml(content, fileName):
   global outDir
-  open(os.path.join(outDir, fileName), "w").write(content.prettify().encode('utf-8'))
+  open(os.path.join(outDir, fileName + ".html"), "w").write(content.prettify().encode('utf-8'))
 
 ##############################################################################
-if len(sys.argv) < 6:
-  print "Usage: " + sys.argv[0] + " <directory with html articles> <images directory> <output directory> <threadIndex> <cpu core count>"
+if len(sys.argv) < 8:
+  print "Usage: " + sys.argv[0] + " <directory with html articles> <images directory> <article set info file> <redirect info file> <output directory> <threadIndex> <cpu core count>"
   exit(1)
 
 inDir = sys.argv[1]
-imageFiles = [unicode((urllib.unquote(file)).lower()) for file in os.listdir(sys.argv[2])]
-outDir = sys.argv[3]
-threadIndex = int(sys.argv[4])
-coreCount = int(sys.argv[5])
-files = [urllib.unquote(file) for file in os.listdir(sys.argv[1])]
+imagesSrcDir = sys.argv[2]
+imageFiles = dict([(transformString(file), file) for file in os.listdir(imagesSrcDir)])
+idMapping = dict([(unicode(i.split("\t")[1]), unicode(i.split("\t")[0])) for i in open(sys.argv[3])])
+redirectMapping = dict([(unicode(line.split("\t")[1]), unicode(line.split("\t")[3].strip())) for line in open(sys.argv[4])])
+outDir = sys.argv[5]
+threadIndex = int(sys.argv[6])
+coreCount = int(sys.argv[7])
+files = [urllib.unquote(file) for file in idMapping.values()]
 thisFiles = files[threadIndex * len(files) / coreCount : (threadIndex + 1) * len(files) / coreCount]
+imageSet = set()
+
+if not os.path.exists(outDir):
+  os.makedirs(outDir)
 
 for file in thisFiles:
   soup = BeautifulSoup(open(os.path.join(inDir, file)))
   soup = cleanUp(soup)
   rewriteImages(soup)
+  rewriteCrossLinks(soup)
   writeHtml(soup, file)
+imagesDstDir = os.path.join(outDir, "images")
+if not os.path.exists(imagesDstDir):
+  os.makedirs(imagesDstDir)
+
+for image in imageSet:
+  shutil.copy2(os.path.join(imagesSrcDir, imageFiles[image]), os.path.join(imagesDstDir, image))
