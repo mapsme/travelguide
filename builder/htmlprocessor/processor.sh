f6531b1d74919664fdeba69eafeee5b9abc45f7a
Alexander Zolotarev <alexander.zolotarev@gmail.com>
2013-08-09 01:25:24 +0300

    Insert map links into geocoded articles.

--- builder/htmlprocessor/processor.sh
+++ builder/htmlprocessor/processor.sh
@@ -1,6 +1,5 @@
 
 for interval in 0 1 2 3 4 5 6 7;
-  do python strip.py $1 $2 $3 $4 $5 $interval 8 &
+  do python strip.py $1 $2 $3 $4 $5 $6 $interval 8 &
   done
 wait
-  
--- builder/htmlprocessor/strip.py
+++ builder/htmlprocessor/strip.py
@@ -11,6 +11,12 @@ sys.setdefaultencoding('utf-8')
 
 from strip_function import cleanUp
 
+def insertMapLink(soup, lat, lon, title, pageId):
+  hrefLink = "mapswithme://map?v=1&ll=%s,%s&n=%s&id=%s&backurl=guideswithme&appname=Guides%%20With%%20Me"%(lat, lon, urllib.quote(title), pageId)
+  mapTag = BeautifulSoup().new_tag("a", href=hrefLink)
+  mapTag["class"] = "geolink";
+  soup.body.append(mapTag)
+
 def transformStringWithEncoding(str):
   return urllib.unquote(str.decode("latin-1").encode("utf-8"))
 
@@ -86,9 +92,12 @@ def writeHtml(content, fileName):
   global outDir
   open(os.path.join(outDir, fileName + ".html"), "w").write(content.encode('utf-8'))
 
+def fixTitle(title):
+  return title.split('/')[-1].replace('_', ' ')
+
 ##############################################################################
 if len(sys.argv) < 8:
-  print "Usage: " + sys.argv[0] + " <directory with html articles> <images directory> <article set info file> <redirect info file> <output directory> <threadIndex> <cpu core count>"
+  print "Usage: " + sys.argv[0] + " <directory with html articles> <images directory> <article set info file> <redirect info file> <geocoords file> <output directory> <threadIndex> <cpu core count>"
   exit(1)
 
 inDir = sys.argv[1]
@@ -96,12 +105,19 @@ imagesSrcDir = sys.argv[2]
 imageFiles = dict([(unicodeNormalize(file), file) for file in os.listdir(imagesSrcDir)])
 idMapping = dict([(unicode(i.split("\t")[1]), unicode(i.split("\t")[0])) for i in open(sys.argv[3])])
 redirectMapping = dict([(unicode(line.split("\t")[1]), unicode(line.split("\t")[3].strip())) for line in open(sys.argv[4])])
-outDir = sys.argv[5]
-threadIndex = int(sys.argv[6])
-coreCount = int(sys.argv[7])
+coords = dict([(line.split("\t")[0], (line.split("\t")[1], line.split("\t")[2])) for line in open(sys.argv[5])])
+pageIdToTitle = {}
+for key, value in idMapping.iteritems():
+  if value in coords:
+    pageIdToTitle[value] = fixTitle(str(key))
+outDir = sys.argv[6]
+threadIndex = int(sys.argv[7])
+coreCount = int(sys.argv[8])
 files = [urllib.unquote(file) for file in idMapping.values()]
 thisFiles = files[threadIndex * len(files) / coreCount : (threadIndex + 1) * len(files) / coreCount]
 imageSet = set()
+
+# preload coords
 
 if not os.path.exists(outDir):
   os.makedirs(outDir)
@@ -111,6 +127,8 @@ for file in thisFiles:
   soup = cleanUp(soup)
   rewriteImages(soup)
   rewriteCrossLinks(soup)
+  if file in coords:
+    insertMapLink(soup, coords[file][0], coords[file][1], pageIdToTitle[file], file)
   writeHtml(soup, file)
 
 imagesDstDir = os.path.join(outDir, "images")
