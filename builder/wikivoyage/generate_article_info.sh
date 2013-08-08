cc04814757b24c429420b5fa2a00de76c8bd818f
Yury Melnichek <melnichek@gmail.com>
2013-08-08 23:48:40 +0300

    [builder] Prefer jpeg images for thumbnails.

--- builder/wikivoyage/generate_article_info.sh
+++ builder/wikivoyage/generate_article_info.sh
@@ -10,7 +10,7 @@ do
     # Create an empty table.
     $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="DROP TABLE IF EXISTS $REGION"
     $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="CREATE TABLE $REGION \
-        (id int(10) unsigned, gen int(3), id1 int(10), title1 varbinary(255), id2 int(10), title2 varbinary(255), primary key(id))"
+        (id int(10) unsigned, gen int(3), image varbinary(255), id1 int(10), title1 varbinary(255), id2 int(10), title2 varbinary(255), primary key(id))"
 
     # Insert the seed category.
     $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="INSERT INTO $REGION(id, gen) \
@@ -38,12 +38,28 @@ do
         JOIN $REGION r2 ON r1.id1 = r2.id \
         SET r1.id2 = r2.id1, r1.title2 = r2.title1"
 
+    # Update page images.
+    $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="UPDATE $REGION \
+        JOIN page_props ON pp_page = id and pp_propname = 'page_image' \
+        SET image = pp_value"
+    $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="UPDATE $REGION \
+        JOIN imagelinks ON il_from = id \
+        JOIN image ON img_name = il_to \
+        SET image = il_to \
+        WHERE (il_to LIKE '%.jpg' OR il_to LIKE '%.JPG' OR il_to LIKE '%.jpeg' OR il_to LIKE '%.JPEG')
+        AND img_width < 2 * img_height AND img_height < 2 * img_width
+        AND NOT (image LIKE '%.jpg' OR image LIKE '%.JPG' OR image LIKE '%.jpeg' OR image LIKE '%.JPEG')"
+    $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="UPDATE $REGION \
+        JOIN imagelinks ON il_from = id \
+        SET image = il_to \
+        WHERE (il_to LIKE '%.jpg' OR il_to LIKE '%.JPG' OR il_to LIKE '%.jpeg' OR il_to LIKE '%.JPEG')
+        AND NOT (image LIKE '%.jpg' OR image LIKE '%.JPG' OR image LIKE '%.jpeg' OR image LIKE '%.JPEG')"
+
     # Output the final result.
     $MYSQL_BINARY --user=$MYSQL_USER --database=$MYSQL_DATABASE --execute="SELECT \
-        page_id, page_title, page_len, image.pp_value, id1, title1, id2, title2 \
+        page_id, page_title, page_len, image, id1, title1, id2, title2 \
         FROM $REGION \
         JOIN page ON page_id = id \
-        LEFT JOIN page_props image ON image.pp_page = id and image.pp_propname = 'page_image' \
         WHERE page_namespace = 0 AND page_is_redirect = 0 \
         ORDER BY page_title" \
         --skip-column-names > $REGION.info.txt
