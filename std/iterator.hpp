a5f31d5bf210176cae2afd32c29f9bfea45a210c
vng <viktor.govako@gmail.com>
2013-08-08 13:51:36 +0300

    Added some std functions.

--- std/iterator.hpp
+++ std/iterator.hpp
@@ -1,4 +1,6 @@
 #pragma once
 
 #include <iterator>
+
 using std::back_inserter;
+using std::distance;
--- std/map.hpp
+++ std/map.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
 #include <map>
+#include <boost/unordered_map.hpp>
 
 using std::map;
+using boost::unordered_map;
