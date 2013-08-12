c8e55580de4191a8a1504ade2702ed4c97812aac
Alexander Zolotarev <alexander.zolotarev@gmail.com>
2013-08-12 16:21:14 +0300

    Added generation of article's headers with image, breadcrumb and title

--- builder/htmlprocessor/strip.py
+++ builder/htmlprocessor/strip.py
@@ -11,14 +11,53 @@ sys.setdefaultencoding('utf-8')
 
 from strip_function import cleanUp
 
+
 def insertMapLink(soup, lat, lon, title, pageId):
   hrefLink = "mapswithme://map?v=1&ll=%s,%s&n=%s&id=%s&backurl=guideswithme&appname=Guides%%20With%%20Me"%(lat, lon, urllib.quote(title), pageId)
   mapTag = BeautifulSoup().new_tag("a", href=hrefLink)
   mapTag["class"] = "geolink";
-  soup.body.append(mapTag)
+  soup.body.insert(0, mapTag)
+
+
+def insertArticleTitle(soup, articleTitle):
+  titleTag = BeautifulSoup().new_tag("div")
+  titleTag["class"] = "articleHeader"
+  titleTag.append(articleTitle)
+  soup.body.insert(0, titleTag)
+
+
+def insertArticleImage(soup, imagePath):
+  imgTag = BeautifulSoup().new_tag("img", src=imagePath)
+  imgTag["class"] = "articleImage"
+  soup.body.insert(0, imgTag)
+
+
+def insertBreadcrumb(soup, articleTitle, parentTitle, parentLink, grandParentTitle, grandParentLink):
+  tagFactory = BeautifulSoup()
+
+  bcWrapper = tagFactory.new_tag("div")
+  bcWrapper["class"] = "breadcrumbs_wrapper"
+  if (grandParentTitle):
+    grandParentTag = tagFactory.new_tag("a", href=grandParentLink)
+    grandParentTag["class"] = "breadcrumb bc1"
+    grandParentTag.append(grandParentTitle)
+    bcWrapper.append(grandParentTag)
+  if (parentTitle):
+    parentTag = tagFactory.new_tag("a", href=parentLink)
+    parentTag["class"] = "breadcrumb bc2"
+    parentTag.append(parentTitle)
+    bcWrapper.append(parentTag)
+  currTag = tagFactory.new_tag("span")
+  currTag["class"] = "breadcrumb bc3"
+  currTag.append(articleTitle)
+  bcWrapper.append(currTag)
+  soup.body.insert(0, bcWrapper)
+
+
 
 def transformStringWithEncoding(str):
   return urllib.unquote(str.decode("latin-1").encode("utf-8"))
+
 
 def transformString(s):
   unquoted = urllib.unquote(str(s));
@@ -104,6 +143,8 @@ inDir = sys.argv[1]
 imagesSrcDir = sys.argv[2]
 imageFiles = dict([(unicodeNormalize(file), file) for file in os.listdir(imagesSrcDir)])
 idMapping = dict([(unicode(i.split("\t")[1]), unicode(i.split("\t")[0])) for i in open(sys.argv[3])])
+# pageId => [parentId, parentTitle, grandParentId, grandParentTitle], ids and titles can be "NULL"
+ancestors = dict([(i.split("\t")[0], i.strip().split("\t")[4:8]) for i in open(sys.argv[3])])
 redirectMapping = dict([(unicode(line.split("\t")[1]), unicode(line.split("\t")[3].strip())) for line in open(sys.argv[4])])
 coords = dict([(line.split("\t")[0], (line.split("\t")[1], line.split("\t")[2])) for line in open(sys.argv[5])])
 pageIdToTitle = {}
@@ -127,8 +168,22 @@ for file in thisFiles:
   soup = cleanUp(soup)
   rewriteImages(soup)
   rewriteCrossLinks(soup)
+  # insert article "header" - image with breadcrumbs and map link
   if file in coords:
-    insertMapLink(soup, coords[file][0], coords[file][1], pageIdToTitle[file], file)
+    articleTitle = pageIdToTitle[file]
+
+    insertMapLink(soup, coords[file][0], coords[file][1], articleTitle, file)
+
+    insertArticleTitle(soup, articleTitle)
+
+    parentTitle = fixTitle(ancestors[file][1]) if ancestors[file][1] != "NULL" else False
+    parentLink = ancestors[file][0] + ".html" if ancestors[file][0] != "NULL" else False
+    grandParentTitle = fixTitle(ancestors[file][3]) if ancestors[file][3] != "NULL" else False
+    grandParentLink = ancestors[file][2] + ".html" if ancestors[file][2] != "NULL" else False
+    insertBreadcrumb(soup, articleTitle, parentTitle, parentLink, grandParentTitle, grandParentLink)
+
+    insertArticleImage(soup, "header_images/" + file + ".jpg")
+
   writeHtml(soup, file)
 
 imagesDstDir = os.path.join(outDir, "images")
