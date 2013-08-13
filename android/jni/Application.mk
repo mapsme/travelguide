14d60bda2a47e13d691f509496bb21ff0e73a33c
d-kunin <mr.kunin@gmail.com>
2013-08-13 10:59:20 +0300

    [and] Removed debug flags.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,8 +1,6 @@
 APP_STL := gnustl_static
 APP_PLATFORM := android-9
 APP_ABI := all
-# remove in release
-APP_OPTIM := debug
 
 LOCAL_PATH := $(call my-dir)
 APP_CFLAGS += -I$(LOCAL_PATH)/../../3rdparty/boost
