e938a113804155726324cfc98c3615ebaa78f761
Darafei Praliaskouski <komzpa@mapswith.me>
2013-08-15 18:22:55 +0300

    Guide with me

--- builder/htmlprocessor/strip.py
+++ builder/htmlprocessor/strip.py
@@ -16,7 +16,7 @@ from strip_function import *
 
 
 def insertMapLink(soup, lat, lon, title, pageId):
-    hrefLink = "mapswithme://map?v=1&ll=%s,%s&n=%s&id=%s&backurl=guideswithme&appname=Guides%%20With%%20Me" % (lat, lon, urllib.quote(title), pageId)
+    hrefLink = "mapswithme://map?v=1&ll=%s,%s&n=%s&id=%s&backurl=guideswithme&appname=Guide%%20With%%20Me" % (lat, lon, urllib.quote(title), pageId)
     mapTag = BeautifulSoup().new_tag("a", href=hrefLink)
     mapTag["class"] = "geolink"
     soup.body.insert(0, mapTag)
