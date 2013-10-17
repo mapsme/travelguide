925dbe96bcb3e39f5ec4cd4897728e79f54fdbdc
Alexander Zolotarev <alexander.zolotarev@gmail.com>
2013-10-17 23:55:04 +0300

    Use Production configuration for AdHoc, only provisioning profiles are different

--- iOS/build.sh
+++ iOS/build.sh
@@ -10,7 +10,6 @@ PROVISIONING_PROFILE_CONST="${LOCAL_DIRNAME}/Profiles"
 BUILD_CONFIGURATION="Production"
 DATE=$(date +%F)
 APP_DSYM_FOLDER="${PROJECT_BUILDDIR}/APP_AND_DSYM-${DATE}"
-RELEASE_ADHOC="adhoc"
 RESULT_DIR="${PROJECT_BUILDDIR}"
 
 declare arr=(UK Japan Russia Spain Hawaii California Germany Italy France Turkey Switzerland Greece)
@@ -22,32 +21,31 @@ cp "${PROVISIONING_PROFILE_CONST}/"* "${SYSTEM_PROVISIONING_DIR}"
 
 # if launched with any parameter, build adhoc versions
 if [ $# == 1 ]; then
-	BUILD_CONFIGURATION="Release"
-	PROVISIONING_PROFILE="${PROVISIONING_PROFILE_CONST}/GuideWithMe_AdHoc.mobileprovision"
-  RESULT_DIR="${RESULT_DIR}/Release-${DATE}"
- else
- 	RESULT_DIR="${RESULT_DIR}/Production-${DATE}"
+  PROVISIONING_PROFILE="${PROVISIONING_PROFILE_CONST}/GuideWithMe_AdHoc.mobileprovision"
+  RESULT_DIR="${RESULT_DIR}/Adhoc-${DATE}"
+else
+  RESULT_DIR="${RESULT_DIR}/AppStore-${DATE}"
 fi
 
 if [ -d "$APP_DSYM_FOLDER" ]; then
-    rm -rf "${APP_DSYM_FOLDER}"
+  rm -rf "${APP_DSYM_FOLDER}"
 fi
 mkdir -p "$APP_DSYM_FOLDER"
 
 if [ -d "$RESULT_DIR" ]; then
-    rm -rf "${RESULT_DIR}"
+  rm -rf "${RESULT_DIR}"
 fi
 mkdir -p "$RESULT_DIR"
 
 for i in ${arr[@]}
 do
-   if [ $# -lt 1 ]; then
-     PROVISIONING_PROFILE="${PROVISIONING_PROFILE_CONST}/${i}_GuideWithMe_Distribution.mobileprovision"
-   fi
-   xcodebuild -target "${i}" -sdk "${TARGET_SDK}" -configuration "${BUILD_CONFIGURATION}" SYMROOT="${PROJECT_BUILDDIR}/${i}"
-   xcrun -sdk "${TARGET_SDK}" PackageApplication -v "${PROJECT_BUILDDIR}/${i}/${BUILD_CONFIGURATION}-${TARGET_SDK}/${i}.app" -o "${RESULT_DIR}/${i}.ipa" --sign "${DEVELOPER_NAME}" --embed "${PROVISIONING_PROFILE}"
-   cp -r "${PROJECT_BUILDDIR}/${i}/${BUILD_CONFIGURATION}-${TARGET_SDK}/"* "$APP_DSYM_FOLDER"
-   rm -rf "${PROJECT_BUILDDIR}/${i}"
+  if [ $# -lt 1 ]; then
+    PROVISIONING_PROFILE="${PROVISIONING_PROFILE_CONST}/${i}_GuideWithMe_Distribution.mobileprovision"
+  fi
+  xcodebuild -target "${i}" -sdk "${TARGET_SDK}" -configuration "${BUILD_CONFIGURATION}" SYMROOT="${PROJECT_BUILDDIR}/${i}"
+  xcrun -sdk "${TARGET_SDK}" PackageApplication -v "${PROJECT_BUILDDIR}/${i}/${BUILD_CONFIGURATION}-${TARGET_SDK}/${i}.app" -o "${RESULT_DIR}/${i}.ipa" --sign "${DEVELOPER_NAME}" --embed "${PROVISIONING_PROFILE}"
+  cp -r "${PROJECT_BUILDDIR}/${i}/${BUILD_CONFIGURATION}-${TARGET_SDK}/"* "$APP_DSYM_FOLDER"
+  rm -rf "${PROJECT_BUILDDIR}/${i}"
 done
 
 zip -r -9 "${RESULT_DIR}/APP_AND_DSYM.zip" "${APP_DSYM_FOLDER}"
