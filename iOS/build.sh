95079fd0d1f982bd778dfefd798089b4887e76a4
Alexander Zolotarev <alexander.zolotarev@gmail.com>
2014-05-06 10:45:40 +0700

    Added Thailand resources to build production iOS and Android versions

--- iOS/build.sh
+++ iOS/build.sh
@@ -15,7 +15,7 @@ DATE=$(date +%F)
 APP_DSYM_FOLDER="${PROJECT_BUILDDIR}/APP_AND_DSYM-${DATE}"
 RESULT_DIR="${PROJECT_BUILDDIR}"
 
-declare arr=(UK Japan Russia Spain Hawaii California Germany Italy France Turkey Switzerland Greece New_Zealand)
+declare arr=(UK Japan Russia Spain Hawaii California Germany Italy France Turkey Switzerland Greece New_Zealand Thailand)
 
 # copy all provisioning profiles first
 cp "$GUIDES_PROVISIONING_DIR"/* "${SYSTEM_PROVISIONING_DIR}"
